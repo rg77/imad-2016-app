52a76ea570946b886fa5ed393076f4318e827fe9
rg77 <rohanggurve@gmail.com>
2016-09-21 23:06:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+}); 
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -15,6 +15,21 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+app.get('/article-one',function (req,res) {
+    res.send('Article one requested and will be served here');
+});
+
+
+app.get('/article-two',function (req,res) {
+    res.send('Article two requested and will be served here');
+});
+
+
+app.get('/article-three',function (req,res) {
+    res.send('Article three requested and will be served here');
 });
 
 
