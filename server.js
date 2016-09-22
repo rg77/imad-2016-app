0ee39c4faa587f0c2353262a7781746374f48f70
rg77 <rohanggurve@gmail.com>
2016-09-22 11:33:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,13 +23,15 @@ app.get('/article-one',function (req,res) {
 });
 
 
-app.get('/article-two',function (req,res) {
-    res.send('Article two requested and will be served here');
+
+app.get('/article-one',function (req,res) {
+      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 
-app.get('/article-three',function (req,res) {
-    res.send('Article three requested and will be served here');
+
+app.get('/article-one',function (req,res) {
+      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
