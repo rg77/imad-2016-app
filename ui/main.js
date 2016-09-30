e5bb84056802a4b00e923208bdf30ccb6c515397
rg77 <rohanggurve@gmail.com>
2016-09-30 23:12:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+var button = document.getElementByID("button");
+var counter=0;
+button.onCLick = function() {
+    
+    var span=document.getElementById("count");
+    count+=1;
+    span.innerHtml =counter.toString();
+}
