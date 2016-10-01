fe4dcbeaebd735c72ccee7fb4ea1530b95affeb4
rg77 <rohanggurve@gmail.com>
2016-10-01 22:20:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,30 @@
 console.log("js");
 var button = document.getElementById("counter");
-var counter=0;
+
 
 button.onclick = function() {
     
-    var span=document.getElementById("count");
-    counter+=1;
-    span.innerHTML =counter.toString();
+    //create request object
+    var request =  new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+        // everything is good, the response is received
+            if(request.status == 200) {
+                var counter = request.responseText;
+                var span=document.getElementById("count");
+                counter+=1;
+                span.innerHTML =counter.toString();
+            }
+    
+        }
+    
+    
+    };
+
+    //make a request
+    request.open('GET', 'http://rg77.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
 };
