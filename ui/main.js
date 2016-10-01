66020cc3f879f070b950fbc63f4000dd1603193e
rg77 <rohanggurve@gmail.com>
2016-10-01 21:48:18 +0530

    made some changes in ui/main.js and server.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log("js");
 var button = document.getElementById("counter");
 var counter=0;
-alert("button not clicked clicked");
-button.onCLick = function() {
-    alert("button clicked");
+
+button.onclick = function() {
+    
     var span=document.getElementById("count");
     counter+=1;
     span.innerHTML =counter.toString();
