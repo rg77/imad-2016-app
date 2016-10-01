83533c7ce90b7ad1e6ea4f3d0313b3055d856e08
rg77 <rohanggurve@gmail.com>
2016-10-02 00:17:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,6 +34,52 @@ var submit_btn = document.getElementById("submit_btn");
 
 submit_btn.onclick = function() {
     
+    
+    //create request object
+    var request =  new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+        // everything is good, the response is received
+            if(request.status === 200) {
+            var names= request.responseText;
+            name = JSON.parse(names);
+            var list='';            
+            for(var i=0;i<names.length;i++)
+            {
+                    list+='<li>' + names[i] + '</li>';
+            }
+            
+            var ul = document.getElementById("namelist");
+            ul.innerHTML =list;
+            }
+    
+        }
+    
+    
+    };
+
+    //make a request
+    request.open('GET', 'http://rg77.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   var names=["name1","name3","name3","name4"];  
     var list='';
     for(var i=0;i<names.length;i++)
