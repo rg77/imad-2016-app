9175f2cf2d66cea91489af6f5d7cf5bb8bcfa946
rg77 <rohanggurve@gmail.com>
2016-10-02 00:00:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names;
+var names = [];
 app.get('/submit-name/name', function(req,res) {
 
 //get name from request
