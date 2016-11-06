a3588675c08050eb194cd33e8c59746abc212aea
rg77 <rohanggurve@gmail.com>
2016-11-06 11:15:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,8 +68,8 @@ app.get('/articles/:articleId',function (req,res) {
           }
           else {
             
-            //var articleData = result.rows[0];        
-           res.send(JSON.stringify(result.rows[0]));
+            var articleData = result.rows[0];        
+           res.send(JSON.stringify(articleData));
               
           }
           
