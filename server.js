5550e07391eda4e1ce1149e9797d5a0e852f9a64
rg77 <rohanggurve@gmail.com>
2016-11-06 10:57:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -503,7 +503,7 @@ app.get('/articles/:articleName',function (req,res) {
       }
       else {
           if(result.rows.length === 0){
-            res.status(404).send('Article Not Found');
+            res.status(404).send('7676Article Not Found');
               
           }
           else {
