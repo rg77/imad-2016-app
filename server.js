9b72811b44a9562b05623efc85e49a3a1d311a25
rg77 <rohanggurve@gmail.com>
2016-11-06 19:04:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var cyrpto = require('crypto');
 
 var config = {
   host: 'db.imad.hasura-app.io',
@@ -48,6 +49,20 @@ app.get('/test-db', function (req, res) {
    });
 }); 
 
+ 
+
+function hash(input,salt) {
+
+var hashed = crypto.pbkdf2(input, salt, 100000, 512, 'sha512');
+return hashed.toString('hex');
+    
+}
+ 
+app.get('/hash/:input', function (req, res) {
+var hashedString = hash(req.params.input,'this-is-some-random-string');
+res.send(hashedString);
+    
+}); 
  
 
 
