17d280ba9b93636f17be4f2bebb1065d84ce17d7
rg77 <rohanggurve@gmail.com>
2016-11-07 15:59:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,12 +26,17 @@ app.get('/', function (req, res) {
 
 }); 
 
-app.get('/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
 //  res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 
 }); 
 
+app.get('/ui/style.css', function (req, res) {
+//  res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+
+}); 
 
 
 
